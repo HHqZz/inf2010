75860c0d23e70038c195845824686d00a7b14eea
Scampix3 <axelmassucci@gmail.com>
2017-11-07 11:26:30 -0500

    tp5 first commit

---
+++ TP5 (tchoin)/tp5/src/Main.java
@@ -0,0 +1,30 @@
+/**
+ * Created by axmas on 17-11-07.
+ */
+
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args)
+    {
+
+
+    }
+
+
+
+    /*private static void testRemainsValid()
+    {
+        Monceau<Integer> tree = new Monceau();
+        int[] arr = new int[] {13, 10, 20, 15, 22, 23, 21, 11, 7, 8, 12, 4, 5, 6};
+        for (int val : arr) {
+            tree.insert(val);
+            if (!tree.isValid()) {
+                System.out.println("ERREUR: Les critères d'un arbre binaire de recherche ne sont pas toujours respectés.");
+                return;
+            }
+        }
+        System.out.println("Tous les tests sur les critères de validité de l'arbre ont passé!");
+    }*/
+}
---
+++ TP5 (tchoin)/tp5/src/Node.java
@@ -0,0 +1,158 @@
+
+import com.sun.xml.internal.bind.AnyTypeAdapter;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author maitr
+ */
+public class Node {
+
+    public int ordre;
+    public Node parent;
+
+    private int valeur;
+    private ArrayList<Node> enfants;
+
+    public Node(int valeur) {
+        this.valeur = valeur;
+        ordre = 0;
+        this.parent = null;
+        enfants = new ArrayList<Node>();
+    }
+
+    public Node(int valeur, Node parent) {
+        ordre = 0;
+        this.valeur = valeur;
+        this.parent = parent;
+        enfants = new ArrayList<Node>();
+    }
+
+    public int getVal() {
+        return valeur;
+    }
+
+    public ArrayList<Node> getEnfants() {
+        return enfants;
+    }
+
+    public void addEnfant(Node enfant) {
+        enfants.add(enfant);
+    }
+
+    public boolean removeEnfant(Node enfant) {
+        if (enfants.contains(enfant)) {
+            enfants.remove(enfant);
+            return true;
+        }
+        return false;
+    }
+
+    public void removeEnfants(ArrayList<Node> enfants) {
+        this.enfants.removeAll(enfants);
+    }
+
+
+    // permet de fusionner deux arbres de meme ordre
+    public Node fusion(Node autre) throws DifferentOrderTrees {
+
+        if(this.ordre != autre.ordre)   // sils ne sont pas du meme ordre
+             throw new DifferentOrderTrees(); //  on lance l'exception
+
+        if(parent == null) {// si larbe courant est vide
+            if(autre.parent == null)
+            {
+                return null;
+            }
+            return autre ;// On retourne le nouvel arbre qui est le noeud passe en parametre
+        }
+        else
+            if(autre.getVal()<this.getVal()){ // On compare les deux arbres
+                autre.addEnfant(autre);      // le plus grand devient l'enfant du deuxieme arbre
+                autre.ordre++;
+                return autre;
+            }
+            else {
+                this.addEnfant(this); // Dans le cas dune egalite alors le deuxieme arbre devient
+                this.ordre++;
+                return this;
+
+            }
+
+
+    }
+
+    // Permet dechanger deux noeuds par valeur (si relation parent enfant)
+    private void moveUp() {
+
+        int tempVal = parent.getVal(); // on copie le parent
+
+        if(parent!= null){
+            parent.valeur = this.getVal() ;
+            this.valeur = tempVal ;
+        }
+
+    }
+
+    public ArrayList<Node> delete() {
+        if(parent != null) {    // si noeud a des parents
+            this.moveUp();         // move up
+            return this.parent.delete(); // return sur son parent avec la nouvelle valeur
+        }
+        //sinon
+        else
+        {
+            // on met tous les enfants de la racine a null
+            for(int i = 0; i< this.getEnfants().size() ; i++){
+                this.getEnfants().get(i).parent = null ;
+            }
+            return this.getEnfants();
+        }
+
+    }
+
+    public void print(String tabulation) {
+        // on affiche la valeur du noeud courant
+        System.out.println(tabulation);
+        System.out.println(this.getVal());
+
+        for(int i = 0 ; i< this.getEnfants().size() ; i++) { // Pour tous les enfants du noeud courant
+            this.getEnfants().get(i).print(tabulation); // on rappelle print
+            System.out.println("\n");
+
+        }
+    }
+    
+    public Node findValue(int valeur) {
+        //
+        return null;
+    }
+    
+    public ArrayList<Integer> getElementsSorted() {
+        ArrayList<Integer> listeTrie = getElementsSorted();
+
+        listeTrie.add(this.getVal());
+        minIndex(this.getEnfants());
+
+
+
+
+    }
+
+
+    // Retourne une liste qui contient les valeurs tries des enfants dun noeud
+    public static int minIndex (ArrayList<Node> list) {
+        ArrayList listVal = new ArrayList(); // cree nouvelle liste
+        for(int i = 0 ; i<list.size() ; i++) { // pour chaque enfant de la liste de noeud
+            listVal.add(list.get(i).getVal()); // on ajoute la valeur dans la liste de valeur
+        }
+        return listVal.indexOf (Collections.min(listVal));
+    }
+}
